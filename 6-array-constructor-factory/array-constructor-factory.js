2320f6aef40965183c2e8dd5d6aacc16187ffe95
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-12 16:43:43 +0600

    Added Pattern

--- 6-array-constructor-factory/array-constructor-factory.js
+++ 6-array-constructor-factory/array-constructor-factory.js
@@ -1,12 +1,12 @@
 
-//  Array constructor
+//  Array constructor pattern
 // It’s a way to create an array using the built-in Array constructor function.
 
 const ar1 = new Array(5); // [ <5 empty items> ]
 let arr2 = new Array(1, 2, 3); // array with element [ 1, 2, 3 ]
 console.log(arr2); 
 
-// Array Factory
+// Array Factory pattern
 let numbers = [1, 2, 3];
 console.log(numbers);
 
