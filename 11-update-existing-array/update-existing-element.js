02343b1357229efb6a961980827ab36bf927f35d
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-13 01:19:05 +0600

    Update existing element  using index and id

--- 11-update-existing-array/update-existing-element.js
+++ 11-update-existing-array/update-existing-element.js
@@ -3,6 +3,33 @@
 
 
 // easy one
-const numbers = [1,2,3,4,5];
-numbers[2] = 10;
-console.log(numbers);
+const numbers = [1,2,7,4, 5];
+numbers[2] = 3;
+// console.log(numbers);
+
+// array of objects
+const students = [
+    {id:1, name:'Shakil'},
+    {id:2, name:'Noyon'},
+    {id:3, name:'Nadim'},
+    {id:4, name:'Mafuj'},
+];
+
+const updatedId = 3;
+const updatedName = 'Asraf Ali';
+
+students.forEach(student => {
+    if(student.id === updatedId){
+       student.name = updatedName;
+    }
+});
+
+console.log(students);
+
+
+
+
+
+
+
+
