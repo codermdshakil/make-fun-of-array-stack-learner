1bc62553703beb003b41c2b47339614a4730c4e3
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-12 21:41:33 +0600

    Implement : Updated array

--- 8-array-update/array-update.js
+++ 8-array-update/array-update.js
@@ -17,5 +17,31 @@ arr2.fill(5, 3, 8); // after index 3 to 8 array element will be 5
 
 // update using index
 const names = ['shakil', 'noyon', 'nadim'];
-names[0] = false;
+// names[0] = false;
 // console.log(names);
+
+
+// Fill array and update
+const response = new Array(9);
+response.fill(false);
+
+for (let i = 0; i < response.length; i++) {
+    const rand = Math.floor(Math.random() * 10 + 1);
+    response[i] = rand > 5 ? 'X':'O';
+};
+
+// console.log(response);
+
+// array is mutable
+
+function update(arr){
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = `${i+1}. ${arr[i]}`
+    }
+    return arr;
+}
+
+update(names); // [ '1. shakil', '2. noyon', '3. nadim' ]
+// console.log(names);
+
+
