8d3201909ded936b03021844b22fd7c2bd463041
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-12 16:43:11 +0600

    Learn : Array constructor vs factory - factory is more realable and shorthand

---
+++ 6-array-constructor-factory/array-constructor-factory.js
@@ -0,0 +1,17 @@
+
+//  Array constructor
+// It’s a way to create an array using the built-in Array constructor function.
+
+const ar1 = new Array(5); // [ <5 empty items> ]
+let arr2 = new Array(1, 2, 3); // array with element [ 1, 2, 3 ]
+console.log(arr2); 
+
+// Array Factory
+let numbers = [1, 2, 3];
+console.log(numbers);
+
+/**
+ * Array constructor is creating array using new Array 
+ * Array Factory is creating array is just []
+ */
+
