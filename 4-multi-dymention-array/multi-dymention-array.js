e5b4b6e59627bd83ed7c65818f043f9028ce2aee
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-12 16:26:58 +0600

    Learn : Multi Dymentional array

---
+++ 4-multi-dymention-array/multi-dymention-array.js
@@ -0,0 +1,25 @@
+
+// Multi dymentional array
+// - array of array
+// - A multidimensional array is simply an array inside another array.
+// - It’s like a grid, a table, or matrix — rows and columns.
+
+let matrix = [
+    [1, 2, 3],   // row 0
+    [4, 5, 6],   // row 1
+    [7, 8, 9]    // row 2
+];
+
+
+// access multi dymentional array 
+for(let i = 0; i<matrix.length; i++){
+    for (let j = 0; j < matrix.length; j++) {   
+        // console.log(matrix[i][j]); // i -> row, j -> col
+    }
+}
+
+
+
+
+
+
