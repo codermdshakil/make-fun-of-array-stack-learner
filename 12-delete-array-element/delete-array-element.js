2123ca3f35d51ec19152a0055431bb8ac68ec0b4
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-13 01:24:25 +0600

    Delete element from an array using pop, shift

---
+++ 12-delete-array-element/delete-array-element.js
@@ -0,0 +1,24 @@
+
+/**
+ * Delete element from an array
+ * 
+*/
+
+const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+
+//* last element delete from array
+
+numbers.pop();
+
+//* first element delete from an array
+numbers.shift();
+
+
+
+
+
+
+
+
+
+
