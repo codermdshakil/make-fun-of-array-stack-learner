3a324696623db7bb14857e813b1c3c485a7b2168
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-12 16:14:24 +0600

    how array works and array features

---
+++ 2-how-array-works/how-array-works.js
@@ -0,0 +1,18 @@
+
+// how does array work?
+/**
+ * 
+ * 1. create an instance
+ * const names = []
+
+ * 2. add element
+ * names[0] ='shakil'
+ *
+ * 3. access when nesessary
+ * console.log(names[0])
+ *  
+ * 
+ * 
+ * 
+*/
+
