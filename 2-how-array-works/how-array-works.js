09501d91c691d309b90f17af8cc2a42e88f21fc9
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-12 16:17:45 +0600

    Array store: every kinds of data like number, string, array, object, boolean, undefine, NaN everythings

--- 2-how-array-works/how-array-works.js
+++ 2-how-array-works/how-array-works.js
@@ -11,8 +11,11 @@
  * 3. access when nesessary
  * console.log(names[0])
  *  
- * 
- * 
+ * JS array is dynamic
+ * - Don't need to specify array length
+ * - Dynamically increase/decrease array size
+ * - Don't need to collect garbage
+ * - All kind of data we can store in array
  * 
 */
 
---
+++ 3-array-store/array-store.js
@@ -0,0 +1,9 @@
+
+// array store every kind of data like int, string, object, array, function etc
+
+function myFun(){
+    console.log('hello');
+}
+const lists = [1,2,3,4, false, '', {name:'shakil', age:20}, myFun, [1,2,3]];
+console.log(lists);
+
