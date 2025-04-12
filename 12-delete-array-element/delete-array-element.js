bca8eeadd8ab714136d80929ee0aec44544ae048
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-13 01:35:10 +0600

    Delete element from Array: pop, shift, filter, delete, splice, indexOf+splice

--- 12-delete-array-element/delete-array-element.js
+++ 12-delete-array-element/delete-array-element.js
@@ -6,12 +6,40 @@
 
 const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 
-//* last element delete from array
+//* 1. last element delete from array
+// numbers.pop();
 
-numbers.pop();
+//* 2. first element delete from an array
+// numbers.shift();
 
-//* first element delete from an array
-numbers.shift();
+
+//* 3. using Delete
+// delete numbers[4]; 
+
+
+//* 4. using filter one or many using && operator
+
+// const filteredNumbers = numbers.filter((item) => item != 5) 
+// const filteredNumbers = numbers.filter((item) => item != 5 && item != 8 && item != 9);
+// console.log(filteredNumbers);
+
+
+//* 5. using splice we can delete a specific element from array
+// const result = numbers.splice(3, 2); 
+// console.log(result); // result containe deleted element in a array
+// console.log(numbers); // main array will return without deleted item
+
+//* IndexOf + splice
+const indexOfItemSeven = numbers.indexOf(7);
+numbers.splice(indexOfItemSeven, 1);
+// console.log(numbers); // in main numbers array 7 will is deleted
+
+
+
+
+
+
+
 
 
 
