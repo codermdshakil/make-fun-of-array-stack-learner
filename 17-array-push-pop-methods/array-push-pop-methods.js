3682fda0eb0d62bd76b340044122e461af1ff867
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-13 23:59:07 +0600

    Stack VS Array : print Hello -> olleH reverse way using stack vs array here array is more efficient + less code

--- 17-array-push-pop-methods/array-push-pop-methods.js
+++ 17-array-push-pop-methods/array-push-pop-methods.js
@@ -4,6 +4,8 @@
 */
 
 const Stack = require('../16-array-as-stack/array-as-stack');
+
+console.time('Stack: ')
 const stack = new Stack();
 const text = 'Hello';
 
@@ -18,7 +20,35 @@ while(!stack.isEmpty()){
     reversedText += stack.peek();
     stack.pop();
 }
-console.log(reversedText);
+// console.log(reversedText);
+console.timeEnd('Stack: ')
+
+// Now reverse text using js array push, pop
+console.time('Array : ')
+const arr = [];
+const text1 = 'hello';
+for (let i = 0; i < text1.length; i++) {
+    arr.push(text1[i]);
+}
+
+
+let reversedTestArray = '';
+
+for (let i = arr.length - 1 ; i >= 0; i--) {
+    reversedTestArray += arr[i];
+}
+
+// console.log(reversedTestArray);
+console.timeEnd('Array : ')
+
+
+// Time complexity
+// Stack: : 0.29ms
+// Array : : 0.013ms
+
+
+
+
 
 
 
