c82468cc8f975a7a60b4763dce91e9e23632fe23
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-14 01:04:07 +0600

    ForEach implmentation vs forEach method

--- 20-forEach/forEach.js
+++ 20-forEach/forEach.js
@@ -24,21 +24,38 @@ const names = [
 
 // how forEach concepts come
 // forEach [concepts]
-function reuse(arr){
+
+// forEach methos implementation
+console.time('ForEach Manual using Concept: ');
+function reuse(arr, logic){
     for (let i = 0; i < arr.length; i++) {
         logic(arr[i], i, arr);        
     }
 }
 
+function log(value){
+    console.log(value);
+}
+
 function logic(value, index, arr){
     console.log(`Value: ${value}, Index: ${index}, Arr:${arr}`);
 }
-reuse([]);
-reuse(['one', 'two']);
+
+reuse([1, 2], log);
+reuse(['one', 'two', 'three'], logic);
+console.timeEnd('ForEach Manual using Concept: ');
+
+// using forEach methos print names index and array
+console.time('ForEach : ');
+names.forEach((v, idx, arr) =>{
+    console.log(`Value: ${v}, Index: ${idx}, Arr:${arr}`);
+})
+console.timeEnd('ForEach : ');
 
 
-
-
+// Time Complexity here also won forEach method
+// ForEach Manual using Concept: : 15.182ms
+// ForEach : : 2.751ms
 
 
 
