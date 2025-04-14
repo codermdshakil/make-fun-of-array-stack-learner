af10d71298597185b572b04d39d2fe34fad4e64d
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-14 16:06:28 +0600

    Learn : some

--- 24-some-every/some-every.js
+++ 24-some-every/some-every.js
@@ -7,16 +7,20 @@ let productReview = [
 
 const positive = productReview.every((value) => value.review > 3);
 
-if(positive){
-    console.log('100% Positive Reviews!');
-}
-else{
-    console.log('Some customer not satisfied!');
-}
+// if(positive){
+//     console.log('100% Positive Reviews!');
+// }
+// else{
+//     console.log('Some customer not satisfied!');
+// }
 
-let contestRating = [
-  { name: "Md. Ashraf", rating: "1189" },
-  { name: "Jalal Uddin", rating: "632" },
-  { name: "Saiful Islam", rating: "799" },
-  { name: "Md. Rayhan", rating: "1043" },
+let match_runs = [
+  { name: "Md. Ashraf", run: "1189" },
+  { name: "Jalal Uddin", run: "632" },
+  { name: "Saiful Islam", run: "400" },
+  { name: "Md. Rayhan", run: "1043" },
 ];
+
+const atLestOne = match_runs.some((value) => parseInt(value.run) === 400);
+// console.log(atLestOne);
+
