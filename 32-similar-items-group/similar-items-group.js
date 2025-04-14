c2aca7114c9e9dd3d29ff260984b8a9eb9ad7553
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-15 02:14:12 +0600

    Group element

---
+++ 32-similar-items-group/similar-items-group.js
@@ -0,0 +1,32 @@
+
+// Grouping Similar Items
+
+const words = [
+    'One',
+    'Two',
+    'Three',
+    'Four',
+    'Five',
+    'Six',
+    'Seven',
+    'Eight',
+    'Nine',
+    'Ten'
+];
+
+const result = words.reduce((acc, cur) => {
+    const len = cur.length;
+    if(acc[cur]){
+        acc[len].push(cur)
+    }
+    else{
+        acc[len] = [cur];
+    }
+    return acc;
+}, {});
+
+
+console.log(result);
+
+
+
