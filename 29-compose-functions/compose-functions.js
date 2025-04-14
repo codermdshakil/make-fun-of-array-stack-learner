bec4c86df63431ad9a21b29e485585641a70ea99
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-14 22:20:54 +0600

    Compose Function

---
+++ 29-compose-functions/compose-functions.js
@@ -0,0 +1,20 @@
+
+// Compose functions
+
+// Understand compose concepts
+const add10 = (n) => n+10;
+const times3 = (n) => n *3;
+const div2 = (n) => n/2;
+
+const n = 5;
+// concepts
+const x = div2(times3(add10(n)));
+console.log(x);
+
+// Pipe function - shortest way
+const pipe = (...fns) => (x) => fns.reduce((v, f) => f(v), n);
+const r1 = pipe(add10, times3, div2)(n);
+// console.log(r1);
+
+ 
+
