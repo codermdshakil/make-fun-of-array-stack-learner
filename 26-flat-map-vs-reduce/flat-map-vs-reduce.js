4879d2170b2cb14a6470e78b085ed2f691e83c5c
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-14 20:56:38 +0600

    Learn : flatMap method

--- 26-flat-map-vs-reduce/flat-map-vs-reduce.js
+++ 26-flat-map-vs-reduce/flat-map-vs-reduce.js
@@ -10,3 +10,6 @@ const arr = [1,2,3,4];
 // using map + flat method
 const flatMap = arr.map((x) => [x*2]).flat(); // [ 2, 4, 6, 8 ]
 
+// using flatMap method
+const flatMap2 = arr.flatMap((x) => [x*2]); // [ 2, 4, 6, 8 ]
+
