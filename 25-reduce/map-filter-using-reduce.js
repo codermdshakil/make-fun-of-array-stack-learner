35e2f649d9a84ba9027497cbfd1f14981b29aa6e
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-14 22:11:05 +0600

    Map+Filer Vs Map+filter using reduce [WIN]

--- 25-reduce/map-filter-using-reduce.js
+++ 25-reduce/map-filter-using-reduce.js
@@ -25,9 +25,31 @@ const odds = numbers.reduce((acc, cur) => {
 
 
 // Big Array
+const bigArray = [];
+for (let i = 0; i < 5000000; i++) {
+    bigArray.push(i);
+}
 
 
 // Map and Filter Chain - Time Efficiency
+console.time('Filer + Map :');
+const oddsSquare = bigArray.filter((item) => item%2 > 0).map(item => item * item);
+// console.log(oddsSquare);
+console.timeEnd('Filer + Map :');
+
 
 
 // Map and Filter Reduce - Time Efficiency
+console.time('Filer + Map Using Reduce: ');
+const oddsSquareUsingReduce = bigArray.reduce((acc, cur) =>{
+    if(cur%2 > 0){
+        acc.push(cur*cur);
+    }
+    return acc;
+},[]);
+// console.log(oddsSquareUsingReduce);
+console.timeEnd('Filer + Map Using Reduce: ');
+
+// Time efficiency
+// Filer + Map : 1.003s
+// Filer + Map Using Reduce: 208.542ms - WIN
