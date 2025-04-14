d3b2e5766637dff18d2da137542bc380c1540ce1
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-14 22:22:31 +0600

    Compose function in differents ways

--- 29-compose-functions/compose-functions.js
+++ 29-compose-functions/compose-functions.js
@@ -16,5 +16,15 @@ const pipe = (...fns) => (x) => fns.reduce((v, f) => f(v), n);
 const r1 = pipe(add10, times3, div2)(n);
 // console.log(r1);
 
- 
+
+// Compose Function - Long way
+
+const compose = (...fns) => {
+    return x => {
+        return fns.reduce((v, f) => f(v), x);
+    }
+}
+
+const r2 = compose(add10, times3, div2)(n);
+// console.log(r2);
 
