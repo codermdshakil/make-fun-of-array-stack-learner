d93a87f8f7c9aff90f20f4d25577556654ac4b02
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-14 20:58:19 +0600

    Flat map

--- 26-flat-map-vs-reduce/flat-map-vs-reduce.js
+++ 26-flat-map-vs-reduce/flat-map-vs-reduce.js
@@ -13,3 +13,7 @@ const flatMap = arr.map((x) => [x*2]).flat(); // [ 2, 4, 6, 8 ]
 // using flatMap method
 const flatMap2 = arr.flatMap((x) => [x*2]); // [ 2, 4, 6, 8 ]
 
+const flatMap3 = arr.flatMap((x) => [x,x*2]); // [ 1, 2, 2, 4, 3, 6, 4, 8 ]
+// console.log(flatMap3); 
+
+
