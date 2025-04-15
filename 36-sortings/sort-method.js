5db24bfcd61d16f072204b828ae92d13651ff361
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-16 01:45:51 +0600

    Sort : numbers of array in assending order, dessending order

---
+++ 36-sortings/sort-method.js
@@ -0,0 +1,30 @@
+
+// How to work with sort method
+
+// const numbers = [10,20,30,40,50,60,70,80,90,100];
+
+// just sort method will not work perpectly
+// numbers.sort();
+// console.log(numbers);
+
+const numbers = [12, 48, 3, 30, 19, 25, 7, 44, 1, 27, 38, 52, 16, 5, 33];
+
+// assending order 
+// numbers.sort((a, b) => a - b);
+// console.log(numbers);
+
+
+// dessending order 
+// numbers.sort((a, b) => b - a);
+// console.log(numbers);
+
+
+
+
+
+
+
+
+
+
+
