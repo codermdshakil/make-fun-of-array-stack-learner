59cdead814e82a65154845772975ca729da813af
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-16 01:32:02 +0600

    Understand: sorting

---
+++ 36-sortings/understand-sorting.js
@@ -0,0 +1,47 @@
+
+// Understand Sorting
+
+
+const arr = [14, 2,7,4,55,9,28,34];
+// const sortedArr = [2,4,7,9,12,28,34,55];
+
+// Accending order sort
+// arr.sort((a, b) => a - b); 
+
+// Dessending order sort
+arr.sort((a, b) => b - a);
+// console.log(arr);
+
+const names = [
+    'Nayem',
+    'Shegufa',
+    'Salvy',
+    'Badhon',
+    'Asief',
+    'Sakib',
+    'Mehedi',
+    'Tamim',
+    'Riya',
+    'Setu'
+];
+
+
+// Sorting Algorithms
+
+// 1. Bubble sort
+// 2. Selection sort
+// 3. Insertion sort
+// 4. Quick sort
+// 5. Merge sort
+// 6. Heap sort
+// 7. Radix sort
+
+// many more sorting algo are available
+
+
+
+
+
+
+
+
