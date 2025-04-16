77eaa6e7f5ff62024fe0c5356523858cd9a8d8ad
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-16 23:56:46 +0600

    Learn : includes, indexOf, lastIndexOf

---
+++ 38-find-element/includes.js
@@ -0,0 +1,18 @@
+
+// Includes, indexOf, lastIndexOf
+
+const arr = ['one', 'two', 'three', 'four', 'one'];
+
+// check the element exist or not
+// console.log(arr.includes('One')); // true 
+
+// indexOf - give the first matched element index 
+console.log(arr.indexOf('one')); // 1
+
+// indexOf - give the last matched element index 
+// console.log(arr.lastIndexOf('one')); // 4
+
+
+
+
+
