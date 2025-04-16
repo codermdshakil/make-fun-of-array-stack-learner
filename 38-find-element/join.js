12452f1d95b88f0927dbfead51d33c63d54625ff
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-16 23:53:33 +0600

    Learn : Join method

---
+++ 38-find-element/join.js
@@ -0,0 +1,12 @@
+
+const a = ['stack', 'learner', 'and', 'stack', 'school'];
+
+// join methods give a single value and we can say how sentence will be separated using (,-_ ' ')
+
+
+// console.log(a.join('')); // stacklearnerandstackschool
+// console.log(a.join(', ')); // stack,learner,and,stack,school
+// console.log(a.join('-')); // stack-learner-and-stack-school
+
+
+
