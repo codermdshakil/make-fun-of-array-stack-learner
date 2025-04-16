574b39841b41192c564a1e0e2c65c086c7e0c46e
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-17 00:05:01 +0600

    Learn : Compare two array

---
+++ 38-find-element/compare-two-array.js
@@ -0,0 +1,13 @@
+
+// Compare two array
+const arr1 = [1,2,3,4,5];
+const arr2 = [1,2,3,4,5];
+
+// Compare two array equal or not
+if(JSON.stringify(arr1) === JSON.stringify(arr2)){
+    console.log('Equal');
+}
+else{
+    console.log('Not Equal');
+}
+
